236d96ca5b1affd895d0a8a1a4256ddca2ac788b
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-11-24 17:55:54 -0500

    Set up basic counter

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const config = require("../config.json");
+
+const Commando = require("discord.js-commando");
+const client = new Commando.Client();
+
+const path = require("path");
+const sqlite = require("sqlite");
+
+const format = require("string-format");
+
+// Set database provider
+const providerPromise = sqlite.open(path.resolve(__dirname, "./settings.sqlite3")).then(db => {
+	return new Commando.SQLiteProvider(db);
+});
+client.setProvider(providerPromise);
+
+// Register groups/commands/arguments
+client.registry.registerGroups([
+	["word_usage", "Word usage"],
+]);
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(path.resolve(__dirname, "./commands"));
+
+client.on("message", async msg => {
+	for await (const [ targetName, target ] of Object.entries(config.targets)) {
+		if (!target.authors.includes(msg.author.id)) break;
+
+		const match = new RegExp(target.match, "g");
+		const matches = msg.content.match(match);
+		const matchCount = Math.min((matches || []).length, target.maxMatches === -1 ? Infinity : target.maxMatches);
+		if (matchCount === 0) break;
+
+		const oldCount = client.settings.get("count:" + targetName, 0);
+		const newCount = await client.settings.set("count:" + targetName, oldCount + matchCount);
+		
+		const displayRole = msg.guild.roles.get(target.displayRole);
+		displayRole.setName(format(target.displayRoleFormat, {
+			count: newCount,
+		}, `Updated count for ${targetName} target`));
+	}
+});
+
+client.login(config.token);
