f64940d876eae2a734d9915afa4fa01f151df6a6
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-11-25 09:13:30 -0500

    Add resetcount command

---
+++ src/commands/count/resetcount.js
@@ -0,0 +1,26 @@
+const { Command } = require("discord.js-commando");
+
+module.exports = class ResetCountCommand extends Command {
+	constructor(client) {
+		super(client, {
+			args: [{
+				key: "count",
+				label: "count",
+				prompt: "What count should be reset?",
+				type: "string",
+			}],
+			description: "Resets a count to its initial value.",
+			group: "count",
+			memberName: "resetcount",
+			name: "resetcount",
+		});
+	}
+
+	run(msg, args) {
+		return this.client.settings.set("count:" + args.count, 0).then(() => {
+			return msg.reply("The `" + args.count + "` counter has been reset to its initial value.");
+		}).catch(() => {
+			return msg.reply("The `" + args.count + "` counter could not be reset to its initial value.");
+		});
+	}
+};
